f26f1fde5c256aa5a24f8f649e60b02954a804c5
Reza Aliasgari Renani <siavash11sia@gmail.com>
2022-04-19 20:19:14 +0300

    add

---
+++ Rotate_Function/Rotate_Function.ino
@@ -0,0 +1,94 @@
+int photores =A0;
+int photoval;
+int red=9;
+int green=8;
+
+int motor_first_pos=4 ;
+int motor_first_neg=5 ;
+int motor_second_pos=6 ;  
+int motor_second_neg=7 ;
+
+void setup()
+
+{
+  pinMode(photores, INPUT);
+  
+  Serial.begin(9600);
+  pinMode(red,OUTPUT);
+  pinMode(green,OUTPUT);
+
+  
+  pinMode(motor_first_pos, OUTPUT);
+  pinMode(motor_first_neg, OUTPUT);
+  pinMode(motor_second_pos, OUTPUT);
+  pinMode(motor_second_neg, OUTPUT);
+}
+
+
+
+ void Rotate(int direction)
+ 
+ {
+  
+   if (direction = -1) 
+   {
+   
+    digitalWrite(motor_first_pos,LOW);
+    digitalWrite(motor_first_neg,HIGH);
+    digitalWrite(motor_second_pos=6,LOW);
+    digitalWrite(motor_second_neg=7,HIGH);
+   }
+   
+  if (direction = 0)
+  {
+    digitalWrite(motor_first_pos,LOW);
+    digitalWrite(motor_first_neg,LOW);
+    digitalWrite(motor_second_pos=6,LOW);
+    digitalWrite(motor_second_neg=7,LOW);
+
+  }
+  
+  if (direction = +1) 
+  {
+    digitalWrite(motor_first_pos,HIGH);
+    digitalWrite(motor_first_neg,LOW);
+    digitalWrite(motor_second_pos=6,HIGH);
+    digitalWrite(motor_second_neg=7,LOW);
+
+  }
+
+ 
+void loop()
+{
+  
+ photoval = analogRead(photores);
+  Serial.println(photoval);
+  delay(500);
+  
+  if(photoval>350 && photoval=<1000)
+  {
+  
+    Rotate(+1);
+    digitalWrite(green,HIGH);
+    digitalWrite(red,LOW);
+      
+  }
+  
+  
+  if(photoval<350) {
+
+    while ( photoval=<1000)
+    
+    {
+      Rotate (-1)
+      digitalWrite(green,LOW);
+      digitalWrite(red,HIGH);
+    }
+  
+  }
+
+
+else {
+  
+    Rotate(0);
+}
